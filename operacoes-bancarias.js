6363bea3b85909dcc4c89ae517ba549617cfd58d
robertabononi <roberta.bononi16@gmail.com>
2021-02-02 22:50:59 -0300

    operações bancárias

--- operacoes-bancarias.js
+++ operacoes-bancarias.js
@@ -4,11 +4,7 @@ const user = {
     balance: 0
 }
 
-let newTransaction =
-    {
-    type: "credit",
-    value: 50.5
-    }
+//
 
 function createTransaction(transaction) {
     user.transactions.push(transaction)
@@ -21,4 +17,66 @@ function createTransaction(transaction) {
     return user
 }
 
-console.log(createTransaction(newTransaction))
+//
+
+function getHigherTransactionByType(type) {
+
+    let higherTransaction
+    let higherValue = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type == type && transaction.value > higherValue) {
+            higherValue = transaction.value
+            higherTransaction = transaction
+        }
+    }
+
+    return higherTransaction
+}
+
+function getAverageTransactionValue() {
+    
+    let sum = 0
+    let average = 0
+
+    for (let transaction of user.transactions) {
+
+        sum = sum + transaction.value
+
+        average = sum / user.transactions.length
+    }
+
+    return average
+}
+
+function getTransactionsCount() {
+
+    let credit = 0
+    let debit = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type === "credit") {
+            credit = credit++
+        } else {
+            debit= debit++
+        }
+    }
+
+    return {credit, debit}
+}
+
+//
+
+createTransaction({ type: "credit", value: 50 })
+createTransaction({ type: "credit", value: 120 })
+createTransaction({ type: "debit", value: 80 })
+createTransaction({ type: "debit", value: 30 })
+
+console.log(user.balance)
+
+console.log(getHigherTransactionByType("credit"))
+console.log(getHigherTransactionByType("debit"))
+
+console.log(getAverageTransactionValue())
+
+console.log(getTransactionsCount())
